85599fbc56901268dd4e8faa5d1ded8891d373bf
לידור בניסתי <321lidor11@gmail.com>
2022-11-03 19:21:19 +0200

    adding menu

--- components/navBar.jsx
+++ components/navBar.jsx
@@ -1,18 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   Bars4Icon,
   FunnelIcon,
   InboxIcon,
   MagnifyingGlassIcon,
+  XMarkIcon,
 } from "@heroicons/react/20/solid";
 
 function NavBar() {
+  const [menu, setMenu] = useState(false);
   return (
-    <div className="absolute top-0 left-0 w-full h-full text-second pointer-events-none xl:text-first">
-      <div className="sticky top-0 bg-first pt-[20px] xl:w-[351px] xl:bg-second xl:h-screen">
+    <div className="absolute top-0 left-0 w-full h-full text-second pointer-events-none xl:text-first xl:z-10 overflow-hidden">
+      <div className="sticky pointer-events-auto top-0 bg-first pt-[20px] xl:w-[351px] xl:bg-second xl:h-screen">
+        {menu && (
+          <div className="absolute top-0 left-0 w-full h-screen bg-first/90 backdrop-blur-[2px] flex flex-col gap-[60px] items-center justify-center text-[30px]">
+            <div className="flex gap-[10px] items-center">
+              <p>הפרופיל שלי</p>
+              <div className="bg-gradient-to-tr from-second to-third w-[20px] h-[20px] rounded-full shadow-[0px_0px_8px_#FBB752]"></div>
+            </div>
+            <div className="flex gap-[10px] items-center">
+              <p>הגדרות</p>
+              <div className="bg-gradient-to-tr from-second to-third w-[20px] h-[20px] rounded-full shadow-[0px_0px_8px_#FBB752]"></div>
+            </div>
+            <div className="flex gap-[10px] items-center">
+              <p>יציאה</p>
+              <div className="bg-gradient-to-tr from-second to-third w-[20px] h-[20px] rounded-full shadow-[0px_0px_8px_#FBB752]"></div>
+            </div>
+          </div>
+        )}
+        <div className="hidden xl:block xl:absolute xl:left-[355px] xl:w-[2px] xl:h-[100vh] xl:bg-gradient-to-t xl:from-third/0 xl:via-third xl:to-third/0"></div>
         <div className="w-full h-max flex justify-between items-center px-[20px]">
-          <Bars4Icon className="h-[30px]" />
-          <p className="text-[30px] font-bold tracking-[0.2em] text-gradient-to-r from-third/0 via-third to-third/0 sm:text-[50px]">
+          {menu ? (
+            <XMarkIcon
+              className="h-[30px] z-10"
+              onClick={() => setMenu(false)}
+            />
+          ) : (
+            <Bars4Icon className="h-[30px]" onClick={() => setMenu(true)} />
+          )}
+          <p className="text-[30px] font-bold tracking-[0.2em] text-gradient-to-r from-third/0 via-third to-third/0 sm:text-[50px] xl:opacity-0 z-10">
             סתישר
           </p>
         </div>
--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,12 @@ import Post from '../components/post'
 
 export default function Home() {
   return (
-    <div className="bg-first w-full h-screen flex flex-col pt-[140px] overflow-y-auto pb-[40px] sm:pt-[180px] xl:pl-[360px] gap-[30px]">
+    <div className="bg-first w-full h-screen flex flex-col pt-[140px] overflow-y-auto pb-[40px] sm:pt-[180px] xl:pl-[360px] gap-[30px] xl:relative">
+      <div className="hidden xl:block xl:absolute xl:w-full xl:h-[2px] xl:top-[115px] xl:right-0 pl-[350px]">
+        <div className="w-full h-full bg-gradient-to-r from-third/0 via-third to-third/0 xl:flex xl:items-end xl:text-[50px] xl:font-bold xl:justify-end">
+          <p className="hidden xl:block xl:mb-[20px] xl:mr-[20px] xl:text-second xl:tracking-[0.2em]">סתישר</p>
+        </div>
+      </div>
       <Post/>
       <Post/>
       <Post/>
