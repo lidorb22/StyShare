cc686dbd6d285e7cb3e24e760cfc551fee7b2642
לידור בניסתי <321lidor11@gmail.com>
2022-11-06 09:41:34 +0200

    one more

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Post from '../components/post'
 
 export default function Home() {
   return (
-    <div className="bg-first w-full h-screen pt-[140px] flex flex-col overflow-y-auto pb-[20px] sm:pt-[180px] xl:pl-[360px] gap-[30px] xl:relative">
+    <div className="bg-first w-full h-[100vh] pt-[140px] flex flex-col overflow-y-auto pb-[20px] sm:pt-[180px] xl:pl-[360px] gap-[30px] xl:relative">
       <div className="w-full h-full border-red-500 border-[2px] flex flex-col overflow-y-auto">
         <Post/>
         <Post/>
