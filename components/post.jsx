0fb5414491efe28a92786eeeb19e3a99c934c804
לידור בניסתי <321lidor11@gmail.com>
2022-11-08 20:54:03 +0200

    animation to posts

--- components/post.jsx
+++ components/post.jsx
@@ -4,37 +4,91 @@ import {
   ShareIcon,
   ChatBubbleBottomCenterTextIcon,
 } from "@heroicons/react/20/solid";
+import { motion } from "framer-motion";
 
 function Post() {
+  const [focus, setFocus] = useState("off");
   return (
-    <div className="w-full h-max flex justify-between px-[20px] gap-[10px]">
-      {/*icons bar*/}
-      <div className="w-max h-full flex flex-col justify-between items-center text-second py-[10px] xl:h-[160px] xl:self-center">
-        <HeartIcon className="h-[25px]" />
-        <ChatBubbleBottomCenterTextIcon className="h-[25px]" />
-        <ShareIcon className="h-[25px]" />
-      </div>
-      {/*gold glowing strip*/}
-      <div className="w-[2px] h-full bg-gradient-to-t from-third/0 via-third to-third/0"></div>
-      {/*text and info*/}
-      <div className="w-full h-max flex flex-col sm:flex-row sm:gap-[10px] xl:flex-col-reverse">
-        {/*text showing area*/}
-        <div className="bg-second w-full h-max min-h-[130px] rounded-[5px] p-[5px] text-right text-first font-bold text-[16px] xl:text-[20px]">
-          <p className="w-full">
-            ניצחון מוחץ לימין בנוסף לניסיון הרמאות במעטפות הקפולות. לא ניתן להם
-            את התענוג לקחת לנו את המדינה
-          </p>
-        </div>
-        {/*publisher info*/}
-        <div className="w-full h-max flex items-center justify-between text-[14px] text-second/50 font-bold sm:w-max sm:flex-col-reverse sm:text-right sm:h-full xl:flex-row xl:w-full xl:items-end xl:text-[20px]">
-          <p>22/10/2022</p>
-          <div className="flex sm:flex-col-reverse xl:flex-row xl:items-end xl:gap-[10px]">
-            <p>ליפז סטמקאר</p>
-            <div className="hidden sm:block sm:bg-second sm:rounded-[5px] sm:w-[70px] sm:h-[70px] self-end"></div>
+    <motion.div
+      animate={focus}
+      variants={{
+        on: {
+          minHeight: "100%",
+          backgroundColor: "rgb(46, 48, 57)",
+          position: "sticky",
+          top: "0px",
+          bottom: "0px",
+        },
+        off: {
+          bottom: "0px",
+        },
+      }}
+      transition={
+        focus === "on" && {
+          minHeight: { duration: 1.5 },
+        }
+      }
+      onClick={() => (focus === "on" ? setFocus("off") : setFocus("on"))}
+      className="w-full min-h-[30%] h-max"
+    >
+      <div className="w-full h-full flex justify-between px-[20px] gap-[10px]">
+        {/*icons bar*/}
+        <motion.div
+          animate={focus}
+          variants={{
+            on: {
+              height: "150px",
+            },
+          }}
+          className="w-max h-full flex flex-col justify-between items-center text-second py-[10px] xl:h-[160px] xl:self-center"
+        >
+          <div>
+            <HeartIcon className="h-[25px]" />
+          </div>
+          <motion.div
+            animate={focus}
+            variants={{
+              on: {
+                color: "rgb(255 186 82)",
+              },
+            }}
+          >
+            <ChatBubbleBottomCenterTextIcon className="h-[25px]" />
+          </motion.div>
+          <div>
+            <ShareIcon className="h-[25px]" />
+          </div>
+        </motion.div>
+        {/*golden glowing strip*/}
+        <motion.div
+          animate={focus}
+          variants={{
+            on: {
+              height: "150px",
+            },
+          }}
+          className="w-[2px] h-full bg-gradient-to-t from-third/0 via-third to-third/0"
+        ></motion.div>
+        {/*text and info*/}
+        <div className="w-full h-max flex flex-col sm:flex-row sm:gap-[10px] xl:flex-col-reverse">
+          {/*text showing area*/}
+          <div className="bg-second w-full h-max min-h-[130px] rounded-[5px] p-[5px] text-right text-first font-bold text-[16px] xl:text-[20px]">
+            <p className="w-full">
+              ניצחון מוחץ לימין בנוסף לניסיון הרמאות במעטפות הקפולות. לא ניתן
+              להם את התענוג לקחת לנו את המדינה
+            </p>
+          </div>
+          {/*publisher info*/}
+          <div className="w-full h-max flex items-center justify-between text-[14px] text-second/50 font-bold sm:w-max sm:flex-col-reverse sm:text-right sm:h-full xl:flex-row xl:w-full xl:items-end xl:text-[20px]">
+            <p>22/10/2022</p>
+            <div className="flex sm:flex-col-reverse xl:flex-row xl:items-end xl:gap-[10px]">
+              <p>ליפז סטמקאר</p>
+              <div className="hidden sm:block sm:bg-second sm:rounded-[5px] sm:w-[70px] sm:h-[70px] self-end"></div>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </motion.div>
   );
 }
 
--- pages/index.js
+++ pages/index.js
@@ -3,11 +3,7 @@ import Post from '../components/post'
 export default function Home() {
   return (
     <div className="bg-first w-full h-[100vh] pt-[140px] overflow-hidden pb-[20px] sm:pt-[180px] xl:pl-[360px]">
-        <div className="w-full h-full bg-yellow-500 border-[4px] border-red-500 max-h-full overflow-y-auto relative flex flex-col gap-[30px]">
-        <Post/>
-        <Post/>
-        <Post/>
-        <Post/>
+        <div className="w-full h-full  max-h-full overflow-y-auto relative flex flex-col gap-[30px]">
         <Post/>
         <Post/>
         <Post/>
